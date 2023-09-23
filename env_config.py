29aa0bec940637cd0111bcae7fa562a43b231b0f
VongaiMitchell <32877434+VongaiMitchell@users.noreply.github.com>
2023-09-23 12:00:36 +0000

    Update envs used

--- env_config.py
+++ env_config.py
@@ -2,9 +2,8 @@
 
 environments = {
     "HalfCheetah": "HalfCheetahBulletEnv-v0",
-    "Ant": "AntBulletEnv-v0",
     "Hopper": "HopperBulletEnv-v0",
-    "Walker2D": "Walker2DBulletEnv-v0",
+    "InvertedPendulum": "InvertedPendulumBulletEnv-v0",
     "Humanoid": "HumanoidBulletEnv-v0",
     # Add more environments as needed
 }
